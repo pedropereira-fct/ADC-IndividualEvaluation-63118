a5e37823cb325bde8c48f5cfb981e4396fe302a4
pedropereira-fct <145656939+pedropereira-fct@users.noreply.github.com>
2026-04-01 12:09:51 +0100

    Delete src directory

--- src/main/java/pt/unl/fct/di/adc/indeval/util/AuthInputRequest.java
+++
@@ -1,8 +0,0 @@
-package pt.unl.fct.di.adc.indeval.util;
-
-public class AuthInputRequest<T> {
-    public T input;
-    public AuthToken token;
-
-    public AuthInputRequest() { }
-}
--- src/main/java/pt/unl/fct/di/adc/indeval/util/InputRequest.java
+++
@@ -1,7 +0,0 @@
-package pt.unl.fct.di.adc.indeval.util;
-
-public class InputRequest<T> {
-    public T input;
-
-    public InputRequest() { }
-}
